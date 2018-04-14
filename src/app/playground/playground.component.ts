272aa4464a4f036b6f4341b833e68a7d10f10545
zenpylon <ajdecker1022@gmail.com>
2018-04-14 18:53:58 -0500

    add basic gridster

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,7 +1,29 @@
 import { Component } from '@angular/core';
 
+import { GridsterConfig, GridsterItem } from 'angular-gridster2';
+
 @Component({
   templateUrl: 'playground.component.html',
   styleUrls: ['playground.component.css']
 })
-export class PlaygroundComponent { }
+export class PlaygroundComponent {
+  options: GridsterConfig;
+  dashboard: GridsterItem[];
+
+  constructor() {
+    this.options = {
+      displayGrid: 'always',
+      draggable: {
+        enabled: true
+      },
+      resizable: {
+        enabled: true
+      }
+    };
+    this.dashboard = [
+      {cols: 2, rows: 1, y: 0, x: 0},
+      {cols: 2, rows: 2, y: 0, x: 2}
+    ];
+  }
+
+}
--- src/app/playground/playground.module.ts
+++ src/app/playground/playground.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { GridsterModule } from 'angular-gridster2';
+
 import { PlaygroundComponent } from './playground.component';
 import { PlaygroundRoutingModule } from './playground-router.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    GridsterModule,
     PlaygroundRoutingModule
   ],
   declarations: [ PlaygroundComponent ]
